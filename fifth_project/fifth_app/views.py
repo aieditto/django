ac3764c7951a8956cf942b910b0c4e62a6c7aa34
Md Anisul Islam <mdanisul55@gmail.com>
2023-09-16 23:03:03 +0600

    updated

--- fifth_project/fifth_app/views.py
+++ fifth_project/fifth_app/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
 def contact(request):
-    return render (request,'./fifth_app/index.html/', {'author':'Anis', 'age':23})
+    return render (request,'./fifth_app/index.html/', {'author':'Anis', 'age':23, "courses" : [
+        {
+        "id" : 1,
+        "course" : "C",
+        "teacher" : "Rahim"
+        },
+        {
+        "id" : 2,
+        "course" : "C++",
+        "teacher" : "Kahim"
+        },
+        {
+        "id" : 3,
+        "course" : "Python",
+        "teacher" : "Fahim"
+        },
+        ]}) 
+    # writing as a list
