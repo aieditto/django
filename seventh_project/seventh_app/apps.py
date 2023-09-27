5872483420d33226ae1f50630ac27a6a6ae4e6fb
Md Anisul Islam <mdanisul55@gmail.com>
2023-09-27 19:18:49 +0600

    updated_now

---
+++ seventh_project/seventh_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SeventhAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'seventh_app'
---
+++ seventh_project/seventh_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def payment(request):
+    return render (request,'./sventh_apps/payment.html/')
