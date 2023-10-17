16390f11ec8c506b1ab985699dc6ebd937bbbb56
Md Anisul Islam <mdanisul55@gmail.com>
2023-10-18 00:26:15 +0600

    push

--- Python for phitron/6.6_Abstract Classes and abstractmethod.py
+++ Python for phitron/6.6_Abstract Classes and abstractmethod.py
@@ -1,14 +1,27 @@
-class Animal:
+from abc import ABC, abstractclassmethod #abstract class importing
+class Animal(ABC): #abstract method calling  ABC mean Abstract Base Class
+    @abstractclassmethod #abstractmethod use for identifying which method is need to abstract
     def eat(self):
-        print('eating')
+        pass
+    @abstractclassmethod 
     def move(self):
         pass
 
-class Monkey(Animal):
-    def __init__(self,name) -> None:
-        self.category='Monkey'
+class Anis(Animal):
+    def __init__(self, name) -> None:
+        self.category='Anisul'
         self.name=name
         super().__init__()
+    def eat(self):
+        print('from eat method')
+    
+    def move(self):
+        print('Khaifa Halek')
 
-layka = Monkey("lucky")
-layka.eat()
+Anisul=Anis('Khana dao')
+Anisul.eat()
+Anisul.move()
+
+""""@abstractclassmethod is the process or way where you can from django.conf import settings
+a sub class to create the same method which are already on  parent class method.  
+"""
