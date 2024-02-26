ee23606ed7221f3fb855ef66b7249d8232341ed4
Md Anisul Islam <mdanisul55@gmail.com>
2024-02-26 17:45:20 +0600

    upadate

--- seventh_project/seventh_app/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms 
-class contactform(forms.Form):
-    name = forms.CharField()
-    email= forms.EmailField()
--- seventh_project/seventh_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('payment/',views.payment, name="payment_page"),
-    path('about/',views.about, name="about_page"),
-    path('form/',views.form, name="form"),
-    path('djangoform/',views.Djangoform, name="djangoform"),
-]
