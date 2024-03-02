0954707e360871c316c7537fa97dbd6bf3814d89
Md Anisul Islam <mdanisul55@gmail.com>
2024-03-02 19:54:13 +0600

    dd

--- django_form/form_ap/forms.py
+++ django_form/form_ap/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class ContactForm(forms.Form):
     name=forms.CharField(label="Username")
     email=forms.EmailField()
+    age=forms.DateField()
+    check =forms.BooleanField()
+    FOOD= [('B','Burger'),('M','Momos'), ('Bir','Biriani')]
+    food=forms.MultipleChoiceField(choices=FOOD)
