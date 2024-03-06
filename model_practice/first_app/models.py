c7467e7826193b61292de75d05d89674bd4471bf
Md Anisul Islam <mdanisul55@gmail.com>
2024-03-06 11:12:57 +0600

    update

--- model_practice/first_app/models.py
+++ model_practice/first_app/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=20)
-    roll=models.IntegerField()
+    roll=models.IntegerField(primary_key=True)
     mobile=models.IntegerField()
     subject_name=models.CharField(default="Java",max_length=20)
+    
+    def __str__(self):
+        return  f'Student Name: {self.name}, Roll: {self.roll}' 
+        
     
     
