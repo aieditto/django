94e1eee19488412c519065b0580bdd5824ff67f1
Md Anisul Islam <mdanisul55@gmail.com>
2024-03-09 20:15:09 +0600

    update

--- model_practice/first_app/urls.py
+++ model_practice/first_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from .import models
-from .import views
+# from .import views
+from first_app.views import home,delete_student
 
 urlpatterns=[
-    path('',views.home, name="homepage"),
-    path('delete_student/<int:roll>',views.delete_student, name='delete_student')
+    path('',home, name="homepage"),
+    path('delete_student/<int:roll>',delete_student, name='delete_student')
 ]
