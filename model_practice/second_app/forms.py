b5c8ef1e62446b6b565adf6026cdfd4dbaa9c520
Md Anisul Islam <mdanisul55@gmail.com>
2024-03-11 11:32:52 +0600

    update

--- model_practice/second_app/forms.py
+++ model_practice/second_app/forms.py
@@ -12,3 +12,19 @@ class StudentForm(forms.ModelForm):
         #     'address'
         # ]
         fields= '__all__'
+        labels={
+            'roll' : "Roll Number",
+            'name' : 'User Name',
+            'fathers_name':"Father's Name",
+        }
+        widgets={
+            'roll' : forms.TextInput(attrs={'class' : 'btn-primary'}),
+            'name': forms.PasswordInput()
+        }
+        help_texts={
+            'name':'This is the username of user',
+            'roll':'Confirm your roll'
+        }
+        error_messages={
+            'name': {'required':'your name is required'}
+        }
